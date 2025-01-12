fb1119dba289d77e5ef6517498b76bf90503a1ff
Trace Harris <traceh@whiletrace.com>
2025-01-11 21:57:44 -0800

    placed some psuedo-code for initial design of email_generator for project

---
+++ email_search.py
@@ -0,0 +1,43 @@
+import string
+import random 
+
+# email generator
+def email_constructor(num_of_items,length):
+    string = ''
+    email_list = []
+    # email format has two parts the address and domain name
+    # construct string for address 
+    # construct string for domain 
+    # domain label1
+    # domain label2
+    # TLD
+    # concatinate domain label1, domain label2, Tld 
+    # concatintate address and domain name
+    # append email address to email list 
+    # return email list 
+   
+    # value of random character within assi letters
+    char = string.assci_letters[random.randrange(0, len(string.assci_letters))]
+    # construct a string
+
+    # characters in string constructed to follow specific pattern
+    # pattern to follow format of email
+    # characters in string pattern to be random 
+    
+    # string to be appended to list
+ return email_list
+
+    
+
+    pass
+
+# get user input 
+# create a list
+# create a email 
+# email should follow a certain format
+# number of emails generated based user input
+# emails need to be inserted into a collection list, dectionary etc
+# user needs to be able to insert a specific email into the list
+# email to be placed into list @ position specified by the user
+# application to search for specified email within randomized email list
+# application to output time took to find specified email and position email was found
